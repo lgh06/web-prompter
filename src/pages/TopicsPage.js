b72877d4487661ac294c244f5bcf5872bc29451c
Daniel Liu <hnnk@qq.com>
2021-05-15 17:56:55 +0800

    alibaba iot worked.

--- src/pages/TopicsPage.js
+++ src/pages/TopicsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import './App.css'
 import {
   Switch,
@@ -8,10 +8,53 @@ import {
   useParams
 } from "react-router-dom";
 
+// node引入包名
+const iot = require('alibabacloud-iot-device-sdk/dist/alibabacloud-iot-device-sdk.js');
+// 浏览器、微信小程序，支付宝小程序引入./dist编译的js文件
+// js版本下载地址：
+//    https://github.com/aliyun/alibabacloud-iot-device-sdk/
+//  https://github.com/mqttjs/MQTT.js/tree/v3.0.0
+
 // https://reactrouter.com/web/example/nesting
 export default function TopicsPage() {
   let {path, url} = useRouteMatch();
+  useEffect(() => {
 
+    // TODO fingerprintjs, clientjs or their alternatives
+    // TODO multiple devices, as remote control device and display device.
+    // TODO localStorage
+    // TODO username and password , or Google / Github Login
+    const [ productKey, deviceName, deviceSecret] = ['a16ErpTCBWY', '0000', 'cdda15575d6c4cf344e5c12c9e11b823']
+    const device = iot.device({
+      productKey,
+      deviceName,
+      deviceSecret,
+      tls: false,
+      // 支付宝小程序和微信小程序额外需要配置协议参数
+      // "protocol": 'alis://', "protocol": 'wxs://',
+      // `hello!!!`
+    });
+    // get为订阅的信息
+    const sb = `/${productKey}/${deviceName}/user/get`;
+    device.subscribe(sb , null , (...e) => {
+      console.log('inside subscribe', e)
+    });
+    device.on(`connect`, (...e) => {
+      console.log(`connect successfully!`, e);
+      // update 为上报
+      device.publish(`/${productKey}/${deviceName}/user/update`, `hello world!`);
+    });
+    device.on('message', (topic, payload) => {
+      console.log(topic, payload.toString());
+    });
+    // clean up things when component destroyed.
+    // https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup 
+    return () => { 
+      device.unsubscribe(sb, (...e)=>console.log('inside unsubscribe',e));
+      device.end({force: true}, (...e)=>console.log('inside end',e))
+    }
+  }, []); // [] empty array 
+  // see https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
   return (
     <div>
       <h2>Topics</h2>
